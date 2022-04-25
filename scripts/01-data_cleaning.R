d7a8fd7599c10b9b6ce3ccfc6468932b167a0133
Adam Labas <adam.labas@mail.utoronto.ca>
2022-04-25 00:32:48 -0400

    Data Cleaning and Processing as well as the intial Data Section of the Paper.

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -1,36 +1,43 @@
 #### Preamble ####
-# Purpose: Clean the survey data downloaded from [...UPDATE ME!!!!!]
-# Author: Rohan Alexander [CHANGE THIS TO YOUR NAME!!!!]
-# Data: 3 January 2021
-# Contact: rohan.alexander@utoronto.ca [PROBABLY CHANGE THIS ALSO!!!!]
-# License: MIT
+# Purpose: Prepare the 2021 GSS data
+# Author: Adam Labas and Krupali Bhavsar
+# Data: March 20 2022
+# Contact: adam.labas@mail.utoronto.ca and krupali.bhavsar@mail.utoronto.ca
+# License: 
 # Pre-requisites: 
-# - Need to have downloaded the ACS data and saved it to inputs/data
-# - Don't forget to gitignore it!
-# - Change these to yours
-# Any other information needed?
+# - Need to have downloaded the GSS data and saved it to inputs/data
 
 
 #### Workspace setup ####
 # Use R Projects, not setwd().
 library(haven)
 library(tidyverse)
+library(janitor)
+library(tidyr)
+library(dplyr)
+library(knitr)
+library(tidymodels)
+
+
 # Read in the raw data. 
-raw_data <- readr::read_csv("inputs/data/raw_data.csv"
-                     )
-# Just keep some variables that may be of interest (change 
-# this depending on your interests)
-names(raw_data)
+raw_data <- read.csv("inputs/data/raw_data.csv")
 
-reduced_data <- 
-  raw_data %>% 
-  select(first_col, 
-         second_col)
-rm(raw_data)
-         
+# Correct for a bug that changes the first header name.
+names(raw_data)[1]<- "PLAYER"
 
-#### What's next? ####
+write.csv(x = raw_data,
+          file = "inputs/data/raw.csv")
+# Set the Seed
+set.seed(866)
 
 
+data_split <- initial_split(data = raw_data, prop = 0.80)
 
-         
+training_data <- training(data_split)
+write.csv(x = training_data,
+          file = "inputs/data/train.csv")
+
+test_data <- testing(data_split)
+write.csv(x = test_data,
+          file = "inputs/data/test.csv")
+
